70b3410cfb3df02ab5d652b30be7593862bb0634
Awais Mansha <awaismansha1998@gmail.com>
2022-04-11 15:54:22 +0500

    working on it

--- app/src/main/java/com/mycocktails/MainActivity.kt
+++ app/src/main/java/com/mycocktails/MainActivity.kt
@@ -2,10 +2,30 @@ package com.mycocktails
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
+import androidx.appcompat.widget.LinearLayoutCompat
+import java.nio.file.Files.find
 
 class MainActivity : AppCompatActivity() {
+
+    val categoryBtn by lazy { findViewById<Button>(R.id.button5) }
+    val ingredientBtn by lazy { findViewById<Button>(R.id.button6) }
+    val categoryLayout by lazy { findViewById<LinearLayoutCompat>(R.id.categoryLayout) }
+    val ingredientsLayout by lazy { findViewById<LinearLayoutCompat>(R.id.ingredientsLayout) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        categoryBtn.setOnClickListener {
+            categoryLayout.visibility = View.VISIBLE
+            ingredientsLayout.visibility = View.GONE
+        }
+        ingredientBtn.setOnClickListener {
+            ingredientsLayout.visibility = View.VISIBLE
+            categoryLayout.visibility = View.GONE
+        }
     }
 }
